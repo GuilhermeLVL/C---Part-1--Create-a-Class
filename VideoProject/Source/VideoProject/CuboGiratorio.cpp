c33a508cd09776aeff2331587d1ff8480bc9e683
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-12-18 22:05:35 -0300

    .

--- VideoProject/Source/VideoProject/CuboGiratorio.cpp
+++ VideoProject/Source/VideoProject/CuboGiratorio.cpp
@@ -9,7 +9,16 @@ ACuboGiratorio::ACuboGiratorio()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+
+	float VariavelFloat = 0;
+	int32 VariavelInt32 = 0;
+	bool bVariavelBool = false;
+
+
+	CuboMesh = CreateDefaultSubobject<UStaticMeshComponent>("MeshDoCubo");
 }
+
+
 
 // Called when the game starts or when spawned
 void ACuboGiratorio::BeginPlay()
@@ -23,5 +32,13 @@ void ACuboGiratorio::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	CuboMesh->AddWorldRotation(FRotator(0.0, DeltaTime*30.0, 0.0));
+		CuboMesh->AddLocalRotation(FRotator(DeltaTime*15.0,0.0,0.0));
+
+		FuncaoExemplo();
 }
 
+bool ACuboGiratorio::FuncaoExemplo(int32 Parametro1, float Parametro2)
+{
+	return true;
+}
--- VideoProject/Source/VideoProject/CuboGiratorio.h
+++ VideoProject/Source/VideoProject/CuboGiratorio.h
@@ -15,6 +15,19 @@ public:
 	// Sets default values for this actor's properties
 	ACuboGiratorio();
 
+	//Criando um cubo 
+	UStaticMeshComponent* CuboMesh;
+
+	//Criando tipos de variaveis
+	float VariavelFloat	;
+	int32 VariavelInt32 ;
+	bool bVariavelBool ;
+
+	
+
+	//Funcao boolean com parametros
+	bool FuncaoExemplo(int32 Parametro1=0,float Parametro2=0.0f );
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
